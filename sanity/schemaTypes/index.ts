36a4ee9fffb70650efb7120b610ee946dd10b251
Unbreakable When United <austinkurniawan01@gmail.com>
2026-04-05 00:37:47 +0700

    deactivate unnecesary shemas for now

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,6 @@
 import Blogs from './blogs'
 import Gallery from './gallery'
-import banners from './banners'
+// import banners from './banners'
 import members from './members'
-import group from './group'
-export const schemaTypes = [Blogs, Gallery,banners,members,group]
+// import group from './group'
+export const schemaTypes = [Blogs, Gallery,members]
